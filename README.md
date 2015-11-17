ae91b54d624e5b66ccd83408f58253347544b2e0
Benjamin Coe <ben@npmjs.com>
2015-11-17 15:00:48 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# npmo-license
+
+manage your npm On-Site license.
+
+## Installing
+
+```sh
+npm i npmo-license -g
+```
+
+## Usage
+
+```
+npmo-license --email=ben@nnpmjs.com --key=0000-0000-0000-0000
+```
+
+Or, to be prompted for your email and key, simply run:
+
+```
+npmo-license
+```
+
+## License
+
+ISC
---
+++ bin/npmo-license.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+var argv = require('yargs')
+  .usage('$0 <command> [arguments]')
+  .option('e', {
+    alias: 'email',
+    description: 'billing email of npm On-Site license'
+  })
+  .option('k', {
+    alias: 'key',
+    description: 'license key associated with On-Site license'
+  })
+  .help('help')
+  .alias('h', 'help')
+  .version(require('../package.json').version, 'version')
+  .alias('v', 'version')
+  .usage('download an up-to-date version of your npm On-Site license')
+  .argv
+var License = require('../')
+var license = new License()
+var chalk = require('chalk')
+
+if (argv.email && argv.key) {
+  license.userEmail = argv.email.trim()
+  license.licenseKey = argv.key.trim()
+  license.validateLicense(function (err) {
+    if (err) throw err
+    console.log(license.license)
+  })
+} else {
+  console.log('enter the license information provided in your signup email:')
+  license.interview(function () {
+    console.log(license.license)
+  })
+}
+
+process.on('uncaughtException', function (err) {
+  console.log(chalk.red(err.message + '. most likely you entered an incorrect email or key.'))
+})
